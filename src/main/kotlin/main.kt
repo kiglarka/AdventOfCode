ef60b4f364b2e5c51035ceee673214b72bca7006
Bogi Kiss <bogikiss85@gmail.com>
2020-12-05 19:20:41 +0100

    merge main

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,9 +2,9 @@ import day4.Day4
 import day2.Day2
 
 fun main(args: Array<String>) {
-    val day4 = Day4()
-    day4.main()
     val day2 = Day2()
     day2.main()
+    val day4 = Day4()
+    day4.main()
 
 }
