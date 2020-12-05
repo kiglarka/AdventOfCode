1d3b8be6c37f026b70518a707a2a024b250dec86
Bogi Kiss <bogikiss85@gmail.com>
2020-12-05 21:51:35 +0100

    init day5 part1

---
+++ src/main/kotlin/day5/Day5.kt
@@ -0,0 +1,61 @@
+package day5
+
+import java.io.File
+import java.util.stream.IntStream.range
+import kotlin.streams.toList
+
+class Day5 {
+
+    companion object {
+        private const val INPUT_FILE = "src/main/kotlin/day5/input.txt"
+        private val ROW_RANGE = Pair(0, 127)
+        private val SEAT_RANGE = Pair(0, 7)
+    }
+
+    private val boardingPasses : MutableList<BoardingPass> = mutableListOf()
+
+    private fun getMaxId(): Int = boardingPasses.maxOf{ it.seatId }
+
+    private fun onBoardPasses(){
+        getInputList().forEach {line ->
+            val seat = adjustRange(line)
+            val seatId = generateSeatId(seat)
+            boardingPasses.add(BoardingPass(seat,seatId))
+        }
+    }
+
+    private data class BoardingPass(val seat: Pair<Int,Int>, val seatId: Int)
+
+    private fun adjustRange(code: String) : Pair<Int,Int>{
+        var rowRange = ROW_RANGE
+        var seatRange = SEAT_RANGE
+
+        code.forEach { char ->
+            val rowDiff = (rowRange.second - rowRange.first)
+            val seatDiff = (seatRange.second - seatRange.first)
+            val rowMedium = rowDiff / 2
+            val seatMedium = seatDiff / 2
+
+            when (char) {
+                'F' -> rowRange = Pair(rowRange.first, rowRange.first+rowMedium)
+                'B' -> rowRange = Pair(rowRange.second-rowMedium, rowRange.second)
+                'L' -> seatRange = Pair(seatRange.first, seatRange.first+seatMedium)
+                'R' -> seatRange = Pair(seatRange.second-seatMedium, seatRange.second)
+            }
+        }
+        return Pair(rowRange.first,seatRange.first)
+    }
+
+    private fun generateSeatId(seat: Pair<Int,Int>): Int = seat.first * 8 + seat.second
+
+    private fun getInputList(): List<String> = File(INPUT_FILE).useLines { it.toList() }
+
+
+    fun main() {
+        onBoardPasses()
+        println(getMaxId())
+
+    }
+}
+
+
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,19 @@
 import day1.Day1
 import day4.Day4
 import day2.Day2
+import day5.Day5
 
 fun main(args: Array<String>) {
+    /*
     val day1 = Day1()
     day1.main()
     val day2 = Day2()
     day2.main()
     val day4 = Day4()
     day4.main()
+
+     */
+    val day5 = Day5()
+    day5.main()
 
 }
